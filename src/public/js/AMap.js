cd943b5f2a12601a45ef700b13511063894c331c
dengbupapapa <meizhanghao1989@qq.com>
2018-11-27 17:08:29 +0800

    优化第三方脚本加载

--- src/public/js/AMap.js
+++ src/public/js/AMap.js
@@ -1,43 +1,34 @@
+//高德api
+export const AMapAsync=()=>loadScript('https://webapi.amap.com/maps?v=1.4.8&key=99d77e072f09ea55969ce6b5f1593609','AMap');
 
+//高德可视化api
+export const LocaAsync=()=>loadScript('https://webapi.amap.com/loca?v=1.2.0&key=99d77e072f09ea55969ce6b5f1593609','Loca');
 
-export function AMapAsync(){
-
-    return new Promise(function(resolve,reject){
-
-        let url = 'https://webapi.amap.com/maps?v=1.4.8&key=99d77e072f09ea55969ce6b5f1593609';
-        let jsapi = document.createElement('script');
-
-        jsapi.charset = 'utf-8';
-        jsapi.src = url;
-        document.head.appendChild(jsapi);
-
-        jsapi.onload = function(){
-            resolve(window.AMap);
-        }
-        jsapi.onerror  = function(e){
-            reject(e);
-        }
-
-    });
-
-}
-
-
-export function LocaAsync(){
+//加载脚本
+function loadScript(
+    //请求脚本url地址
+    url,
+    //脚本加载后的全局变量名
+    className
+){
 
     return new Promise(function(resolve,reject){
 
-        let url = 'https://webapi.amap.com/loca?v=1.2.0&key=99d77e072f09ea55969ce6b5f1593609';
-        let jsapi = document.createElement('script');
-
-        jsapi.charset = 'utf-8';
-        jsapi.src = url;
-        document.head.appendChild(jsapi);
-
-        jsapi.onload = function(){
-            resolve(window.Loca);
+        //如果已加载过该jsdk
+        if(window[className]){
+            return resolve(window[className])
         }
-        jsapi.onerror  = function(e){
+
+        let script = document.createElement('script');
+
+        script.charset = 'utf-8';
+        script.src = url;
+        document.head.appendChild(script);
+
+        script.onload = function(){
+            resolve(window[className]);
+        };
+        script.onerror = function(e){
             reject(e);
         }
 
